5b4a40313b948f528031bebd575653e41d36c017
Kozo Nishida <kozo.nishida@gmail.com>
2025-06-17 02:08:00 +0900

    Create create_venn.py

---
+++ create_venn.py
@@ -0,0 +1,15 @@
+from matplotlib_venn import venn2
+import matplotlib.pyplot as plt
+
+with open('MetaboLights_InChIKeys.pkl', 'rb') as f:
+    ml = pickle.load(f)
+
+with open('MW_InChIKeys.pkl', 'rb') as f:
+    mw = pickle.load(f)
+
+mlikeyset = set(ml)
+mwikeyset = set(mw)
+plt.figure()
+v=venn2((mlikeyset, mwikeyset), set_labels=('', ''))
+plt.savefig("MLredMWgreen_venn.png")
+plt.close()
