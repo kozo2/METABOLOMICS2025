cf267d3ca930a1bd99920847274fc4bf58b2b105
Kozo Nishida <kozo.nishida@gmail.com>
2025-06-17 17:06:20 +0900

    Update sunburst_classyfire.py

--- python/sunburst_classyfire.py
+++ python/sunburst_classyfire.py
@@ -4,5 +4,8 @@ import pickle
 with open('ml_inchikey_classyfire.pkl', 'rb') as f:
     ml_inchikey_classyfire = pickle.load(f)
 
+with open('mw_inchikey_classyfire.pkl', 'rb') as f:
+    mw_inchikey_classyfire = pickle.load(f)
+
 fig = px.sunburst(ml_inchikey_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class')
 fig.write_image("testpltly.png")
