076d5b077040cf3d309763cc84bfc7a6c934d68d
Kozo Nishida <kozo.nishida@gmail.com>
2025-06-17 19:02:42 +0900

    create 5 types of sunburst plot with same color palette

--- python/sunburst_classyfire.py
+++ python/sunburst_classyfire.py
@@ -1,20 +1,36 @@
 import plotly.express as px
+from plotly.colors import qualitative
 import pickle
+import itertools
 
-# with open('ml_inchikey_classyfire.pkl', 'rb') as f:
-#     ml_inchikey_classyfire = pickle.load(f)
+with open('ml_inchikey_classyfire.pkl', 'rb') as f:
+    ml_inchikey_classyfire = pickle.load(f)
 
-# with open('mw_inchikey_classyfire.pkl', 'rb') as f:
-#     mw_inchikey_classyfire = pickle.load(f)
+with open('mw_inchikey_classyfire.pkl', 'rb') as f:
+    mw_inchikey_classyfire = pickle.load(f)
 
 with open('intersection_classyfire.pkl', 'rb') as f:
-    df = pickle.load(f)
+    intersection_classyfire= pickle.load(f)
 
-# with open('ml_only_classyfire.pkl', 'rb') as f:
-#     ml_only_classyfire = pickle.load(f)
+with open('ml_only_classyfire.pkl', 'rb') as f:
+    ml_only_classyfire = pickle.load(f)
 
-# with open('mw_only_classyfire.pkl', 'rb') as f:
-#     mw_only_classyfire = pickle.load(f)
+with open('mw_only_classyfire.pkl', 'rb') as f:
+    mw_only_classyfire = pickle.load(f)
 
-fig = px.sunburst(df, path=['kingdom', 'super_class', 'class'], values=None, color='super_class')
-fig.write_image("classyfire_sunburst.png")
+all_super_class = set(ml_inchikey_classyfire['super_class']) | set(mw_inchikey_classyfire['super_class']) 
+palette = itertools.cycle(qualitative.Set3)
+
+color_map = {val: color for val, color in zip(all_super_class, palette)}
+
+fig_ml = px.sunburst(ml_inchikey_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class', color_discrete_map=color_map)
+fig_ml.write_image("ml_classyfire.png")
+fig_mw = px.sunburst(mw_inchikey_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class', color_discrete_map=color_map)
+fig_mw.write_image("mw_classyfire.png")
+fig_intersection = px.sunburst(intersection_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class', color_discrete_map=color_map)
+fig_intersection.write_image("intersection_classyfire.png")
+fig_mlonly = px.sunburst(ml_only_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class', color_discrete_map=color_map)
+fig_mlonly.write_image("mlonly_classyfire.png")
+fig_mwonly = px.sunburst(mw_only_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class', color_discrete_map=color_map)
+fig_mwonly.write_image("mwonly_classyfire.png")
+
