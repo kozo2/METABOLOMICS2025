48dd9cf5dbca78445dbc089f0a66369ff750828b
Kozo Nishida <kozo.nishida@gmail.com>
2025-06-17 18:07:57 +0900

    Update sunburst_classyfire.py

--- python/sunburst_classyfire.py
+++ python/sunburst_classyfire.py
@@ -1,20 +1,20 @@
 import plotly.express as px
 import pickle
 
-with open('ml_inchikey_classyfire.pkl', 'rb') as f:
-    ml_inchikey_classyfire = pickle.load(f)
+# with open('ml_inchikey_classyfire.pkl', 'rb') as f:
+#     ml_inchikey_classyfire = pickle.load(f)
 
-with open('mw_inchikey_classyfire.pkl', 'rb') as f:
-    mw_inchikey_classyfire = pickle.load(f)
+# with open('mw_inchikey_classyfire.pkl', 'rb') as f:
+#     mw_inchikey_classyfire = pickle.load(f)
 
 with open('intersection_classyfire.pkl', 'rb') as f:
-    intersection_classyfire = pickle.load(f)
+    df = pickle.load(f)
 
-with open('ml_only_classyfire.pkl', 'rb') as f:
-    ml_only_classyfire = pickle.load(f)
+# with open('ml_only_classyfire.pkl', 'rb') as f:
+#     ml_only_classyfire = pickle.load(f)
 
-with open('mw_only_classyfire.pkl', 'rb') as f:
-    mw_only_classyfire = pickle.load(f)
+# with open('mw_only_classyfire.pkl', 'rb') as f:
+#     mw_only_classyfire = pickle.load(f)
 
-fig = px.sunburst(ml_inchikey_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class')
-fig.write_image("testpltly.png")
+fig = px.sunburst(df, path=['kingdom', 'super_class', 'class'], values=None, color='super_class')
+fig.write_image("classyfire_sunburst.png")
