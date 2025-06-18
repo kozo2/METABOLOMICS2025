cc84fcfbb694ac72aa6246ab7afecb5d0df64841
Kozo Nishida <kozo.nishida@gmail.com>
2025-06-19 03:59:53 +0900

    Update read_all_ratio_tsvs.py

--- python/read_all_ratio_tsvs.py
+++ python/read_all_ratio_tsvs.py
@@ -1,13 +1,21 @@
 import glob
 import pandas as pd
 
+mltsvs = glob.glob("MTBLS*.tsv")
+mlratios = []
+for i in mltsvs:
+    df = pd.read_csv(i, sep='\t', index_col=0)
+    first_col = df.iloc[:, 0]
+    mlratios.append(first_col)
+giantml = pd.concat(mlratios, axis=1)
+giantml.to_pickle("all_ml_ratio.pkl")
+
 mwtsvs = glob.glob("AN*.tsv")
 mwratios = []
 for i in mwtsvs:
     df = pd.read_csv(i, sep='\t', index_col=0)
     first_col = df.iloc[:, 0]
     mwratios.append(first_col)
-
 giantmw = pd.concat(mwratios, axis=1)
 giantmw.to_pickle("all_mw_ratio.pkl")
 
