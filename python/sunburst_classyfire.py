e8432d6dd8ce1fbbe1d6fb7c038ecce72c140b49
Kozo Nishida <kozo.nishida@gmail.com>
2025-06-19 14:46:28 +0900

    Update sunburst_classyfire.py

--- python/sunburst_classyfire.py
+++ python/sunburst_classyfire.py
@@ -18,7 +18,11 @@ with open('ml_only_classyfire.pkl', 'rb') as f:
 with open('mw_only_classyfire.pkl', 'rb') as f:
     mw_only_classyfire = pickle.load(f)
 
-all_super_class = set(ml_inchikey_classyfire['super_class']) | set(mw_inchikey_classyfire['super_class']) 
+with open('mlmw_inchikey_classyfire.pkl', 'rb') as f:
+    mlmwinchikeys_classyfire = pickle.load(f)
+
+# all_super_class = set(ml_inchikey_classyfire['super_class']) | set(mw_inchikey_classyfire['super_class'])
+all_super_class = set(mlmwinchikeys_classyfire['super_class'])
 palette = itertools.cycle(qualitative.Set3)
 
 color_map = {val: color for val, color in zip(all_super_class, palette)}
@@ -33,4 +37,5 @@ fig_mlonly = px.sunburst(ml_only_classyfire, path=['kingdom', 'super_class', 'cl
 fig_mlonly.write_image("mlonly_classyfire.png")
 fig_mwonly = px.sunburst(mw_only_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class', color_discrete_map=color_map)
 fig_mwonly.write_image("mwonly_classyfire.png")
-
+fig_mlmw = px.sunburst(mlmwinchikeys_classyfire, path=['kingdom', 'super_class', 'class'], values=None, color='super_class', color_discrete_map=color_map)
+fig_mlmw.write_image("mlmw_classyfire.png")
